cf879e4cbd43da35c3e308eeb097ebae48056005
Yichen Xu <ichisaur@gmail.com>
2016-02-20 11:01:02 -0600

    Clean up code, fix style

--- textjam.py
+++ textjam.py
@@ -1,21 +1,20 @@
 from flask import Flask, request, redirect
 from gmusicapi import Mobileclient
 import twilio.twiml
-import classes
+from classes import Song, User
 
 PLAYLISTNAME = 'textJam'
-isPlaylistCreated = False;
+isPlaylistCreated = False
 
 
 api = Mobileclient()
-#implement a solution to have user login
+# implement a solution to have user login
 logged_in = api.login('jusanden7@gmail.com', 'zpewalrdeytbdhmu', Mobileclient.FROM_MAC_ADDRESS)
 if (logged_in):
     print("logged in")
 else:
     print("Log in failed, please verify login details")
 
-    
 
 def parseQuery(query):
     results = api.search_all_access(query, 10)
@@ -23,11 +22,13 @@ def parseQuery(query):
     parsedTitle = results['song_hits'][0]['track']['title']
     parsedArtist = results['song_hits'][0]['track']['artist']
     parsedLength = results['song_hits'][0]['track']['durationMillis']
-    return classes.Song(parsedTitle, parsedArtist, parsedLength, parsedID)
+    return Song(parsedTitle, parsedArtist, parsedLength, parsedID)
+
 
 def createPlaylist():
     playlistID = api.create_playlist(PLAYLISTNAME, 'none', True)
     isPlaylistCreated = True;
+
 
 def deletePlaylist():
     if (isPlaylistCreated):
