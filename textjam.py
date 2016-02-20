4be809728a88ac2e54cf6b0437288b55ae5fd411
Yichen Xu <ichisaur@gmail.com>
2016-02-20 02:26:54 -0600

    Add login statement

--- textjam.py
+++ textjam.py
@@ -3,10 +3,17 @@ from gmusicapi import Mobileclient
 import twilio.twiml
 import classes
 
+api = Mobileclient()
+#implement a solution to have user login
+logged_in = api.login('jusanden7@gmail.com', 'zpewalrdeytbdhmu')
+if !logged_in:
+    print("Log in failed, please verify login details")
+else: 
+    print("logged in")
 
 def textJam():
-    ''insert function here
+
 
 if __name__ = "__main__":
     textjam()
-    
+
