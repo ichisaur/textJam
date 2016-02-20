d07dd810deef00684396988d2e708118e04eadff
Yichen Xu <ichisaur@gmail.com>
2016-02-20 02:54:20 -0600

    Add test code. Remove unfinished code. Issue with parseQuery found

--- textjam.py
+++ textjam.py
@@ -3,23 +3,29 @@ from gmusicapi import Mobileclient
 import twilio.twiml
 import classes
 
+
+
 api = Mobileclient()
 #implement a solution to have user login
-logged_in = api.login('jusanden7@gmail.com', 'zpewalrdeytbdhmu')
-if !logged_in:
+logged_in = api.login('jusanden7@gmail.com', 'zpewalrdeytbdhmu', Mobileclient.FROM_MAC_ADDRESS)
+if (logged_in):
     print("Log in failed, please verify login details")
 else: 
     print("logged in")
 
-def playsong(songID):
 def parseQuery(query):
     results = Mobileclient.search_all_access(query, 20)
     parsedID = results['song_hits'][1]['track']['storeID']
     parsedTitle = results['song_hits'][1]['track']['title']
     return Song(parsedTitle, parsedID)
 
+    
+s = parseQuery('Yellow')
+disp(s.title)
 
 
-if __name__ = "__main__":
-    textjam()
+
+
+
+
 
