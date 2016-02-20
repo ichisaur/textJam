a8e8230d00d3c394a9d951f98689fd7f1af0e90c
Yichen Xu <ichisaur@gmail.com>
2016-02-20 01:49:45 -0600

    Add class file with Song and User classes

---
+++ classes.py
@@ -0,0 +1,29 @@
+class Song():
+
+    """represents the songs taht are passed to gplay music"""
+
+
+    def __init__(self, title, songID):
+        self.title = title
+        self.songID = songID
+        self.votes = 0
+
+    def vote(self): 
+        self.votes += 1
+
+class User():
+
+    """Stores user data"""
+
+    def __init__(self, phoneNumber):
+        self.userID = phoneNumber
+        self.votedSongs = [];
+
+    def voteSong(self, songID):
+        if myItem in votedSongs:
+            self.votedSongs.append(songID)
+            return 1
+        else:
+            return 0
+
+
