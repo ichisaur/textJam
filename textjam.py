7291cb90e2f7436a587917e6d9a7bb635f94f9b3
Yichen Xu <ichisaur@gmail.com>
2016-02-20 20:17:44 -0600

    Backup Version before Twilio Nonesense

--- textjam.py
+++ textjam.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect
 from gmusicapi import Mobileclient
-import twilio.twiml
 from classes import Song, User
+import threading
+import time
+from threading import Timer
 
 PLAYLISTNAME = 'textJam'
 isPlaylistCreated = False
@@ -9,10 +11,28 @@ loginID = 'jusanden7@gmail.com'
 authToken = 'zpewalrdeytbdhmu'
 defaultQuery1 = "Mr. Brightside"
 defaultQuery2 = "Ho Hey"
+currentSongLength = 15
+stopPlayback = False
+
+
+class timerThread (threading.Thread):
+    def __init__(self, threadID):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+
+    def run(self):
+        global stopPlayback
+        global currentSongLength
+        while not stopPlayback:
+            temp = currentSongLength
+            currentSongLength = int(topVotedSong(songList).duration)/1000
+            done = addTopSong(songList)
+            if done:           
+                time.sleep(temp)
 
 
 def parseQuery(query):
-    results = api.search_all_access(query, 10)
+    results = api.search_all_access(query, 50)
     parsedID = results['song_hits'][0]['track']['storeId']
     parsedTitle = results['song_hits'][0]['track']['title']
     parsedArtist = results['song_hits'][0]['track']['artist']
@@ -54,7 +74,7 @@ def listContains(song, listOfSongs):
 
 
 def topVotedSong(listOfSongs):
-    topSong = listOfSongs[1]
+    topSong = listOfSongs[0]
     for i in listOfSongs:
         if i.votes >= topSong.votes:
             topSong = i
@@ -62,8 +82,12 @@ def topVotedSong(listOfSongs):
 
 
 def addTopSong(songList):
-    api.add_songs_to_playlist(playlistID, topVotedSong(songList).songID)
-    topVotedSong(songList).votes = -1
+    if topVotedSong(songList).votes > -1:
+        api.add_songs_to_playlist(playlistID, topVotedSong(songList).songID)
+        topVotedSong(songList).votes = -1
+        return 1
+    else:
+        return 0
 
 
 def returnSong(song, songList):
@@ -72,6 +96,28 @@ def returnSong(song, songList):
             return i
     else:
         return song
+
+
+def loopFunction():
+    # currentSongLength = (int(topVotedSong(songList).duration))/10000
+    global stopPlayback
+    stopPlayback = False
+    while not stopPlayback:
+        userQuery = input('Input song keyword: ')
+        if userQuery == '#stop':
+            stopPlayback = True
+            break
+        s = parseQuery(userQuery)
+        if not listContains(s, songList):
+            songList.append(s)
+            s.vote()
+        else:
+            s = returnSong(s, songList)
+            if not s.votes == -1:
+                s.vote()
+        print(s.votes)
+
+
 
 
 api = Mobileclient()
@@ -87,35 +133,18 @@ createPlaylist()
 
 songList = []
 queuedSongList = []
-stopPlayback = False
 
-#def loopfunction
-#system alarm timefornext song
-while not stopPlayback:
-    userQuery = input('Input song keyword: ')
-    if userQuery == '#stop':
-        stopPlayback = True
-        break
-    s = parseQuery(userQuery)
-    if not listContains(s, songList):
-        songList.append(s)
-        s.vote()
-    else:
-        s = returnSong(s, songList)
-        if not s.votes == -1:
-            s.vote()
-    print(s.votes)
+s1 = parseQuery(defaultQuery1)
+s2 = parseQuery(defaultQuery2)
+songList.append(s1)
+songList.append(s2)
+addSong(s1)
+s2.votes = 10
+currentSongLength = int(s1.duration)/1000
+delayLoop = timerThread(1)
+delayLoop.start()
+
+loopFunction()
+
 deletePlaylist()
 
-#loopfunction
-
-
-
-# MainLoop
-"""
-running = True
-while running:
-    while noinput:
-
-"""
-# make wait sequence by getting system time and adding onto system time with voting system?
